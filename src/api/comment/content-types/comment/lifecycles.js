85b6e4d0ebd743cdf4c74688c33cef6d2c825bd9
Hubert <hubert.grabke@gmail.com>
2024-11-16 23:41:32 +0100

    Update comment lifecycle to support comment count update after deletion

--- src/api/comment/content-types/comment/lifecycles.js
+++ src/api/comment/content-types/comment/lifecycles.js
@@ -15,18 +15,36 @@ module.exports = {
   async afterCreate(event) {
     await handleCommentChange(event, "afterCreate");
   },
-
-  async afterDelete(event) {
-    await handleCommentChange(event, "afterDelete");
+  async beforeDelete(event) {
+    await handleCommentChange(event, "beforeDelete");
   },
 };
 
 async function handleCommentChange(event, lifecyclePhase) {
-  const {data} = event.params;
+  const { data } = event.params;
 
-  const reviewDocumentId = await fetchReviewDocumentId(
-    data?.review?.connect[0]?.id
-  );
+  let reviewDocumentId = "";
+  if (lifecyclePhase === "beforeDelete") {
+    const comment = await strapi.documents("api::comment.comment").findFirst({
+      filters: {
+        id: {
+          $eq: event.params.where.id,
+        },
+      },
+
+      populate: {
+        review: true,
+      },
+    });
+    reviewDocumentId = comment?.review?.documentId
+      ? comment?.review?.documentId
+      : "";
+  } else {
+    reviewDocumentId = await fetchReviewDocumentId(
+      data?.review?.connect[0]?.id
+    );
+  }
+
   if (reviewDocumentId) {
     const commentCount = await strapi.documents("api::comment.comment").count({
       filters: {
@@ -40,7 +58,7 @@ async function handleCommentChange(event, lifecyclePhase) {
 
     await strapi.documents("api::review.review").update({
       documentId: reviewDocumentId,
-      data: {commentCount: commentCount},
+      data: { commentCount: commentCount },
     });
   }
 }
